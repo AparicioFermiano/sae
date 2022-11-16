cead9fd83c9e661ea08a24a055a8e3cb83c9c360
AparicioFermiano <aparicio.empresarial@gmail.com>
2022-11-16 20:08:06 -0300

    teste teste

--- src/main/java/software/Confirm.java
+++ src/main/java/software/Confirm.java
@@ -12,7 +12,7 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
+import java.io.IOException;dfdfdf
 
 import javax.swing.SwingConstants;
 import javax.imageio.ImageIO;
